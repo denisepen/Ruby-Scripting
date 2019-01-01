2fdefae49362920a26eab07d6bc77e8b30f9be71
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-12-31 19:42:05 -0500

    first commit

---
+++ differences.rb
@@ -0,0 +1,20 @@
+
+unless ARGV.length == 2
+  puts "Usage: differences.rb old-inventory new-inventory"
+  exit
+end
+
+old_inventory = File.open(ARGV[0]).collect do | line |
+  line.downcase
+end
+
+new_inventory = File.open(ARGV[1]).collect do | line |
+  line.downcase
+end
+
+puts "The following files have been added:"
+puts new_inventory - old_inventory
+
+puts ""
+puts "The following files have been deleted:"
+puts old_inventory - new_inventory
